4bbe74ab39049ec296d9f7cf5dfd74ce2d0aced9
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-04-29 17:23:03 +0300

    Base template

--- fudge/fudge/settings.py
+++ fudge/fudge/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'fudge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["fudge/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
